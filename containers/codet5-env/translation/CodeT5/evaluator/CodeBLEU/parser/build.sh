456006105d5ab8edc688d89f945633a594595036
Eray Erer <eray.erer@hotmail.com>
2024-04-05 10:29:55 +0300

    truncated build.sh

--- containers/codet5-env/translation/CodeT5/evaluator/CodeBLEU/parser/build.sh
+++ containers/codet5-env/translation/CodeT5/evaluator/CodeBLEU/parser/build.sh
@@ -1,3 +1,4 @@
+git clone https://github.com/tree-sitter/tree-sitter-python
 git clone https://github.com/tree-sitter/tree-sitter-java
 git clone https://github.com/tree-sitter/tree-sitter-c-sharp
 python3 build.py
