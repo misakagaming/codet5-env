91a38da960fbb6b63c0cad19227862adfe0f3838
Eray Erer <eray.erer@hotmail.com>
2024-08-03 11:46:47 +0300

    removed translation and updated dockerfile

--- containers/codet5-env/translation/CodeT5/evaluator/CodeBLEU/parser/build.sh
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/tree-sitter/tree-sitter-python
-git clone https://github.com/tree-sitter/tree-sitter-java
-git clone https://github.com/tree-sitter/tree-sitter-c-sharp
-python3 build.py
