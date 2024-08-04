ae307c9c84d8a11b766988ad18dad33ecc1b5737
Eray Erer <eray.erer@hotmail.com>
2024-08-04 21:33:27 +0300

    added perl and ruby

--- containers/codet5-env/CodeT5+/evaluator/CodeBLEU/parser/build.sh
+++ containers/codet5-env/CodeT5+/evaluator/CodeBLEU/parser/build.sh
@@ -2,4 +2,6 @@ git clone https://github.com/tree-sitter/tree-sitter-python
 git clone https://github.com/tree-sitter/tree-sitter-java
 git clone https://github.com/tree-sitter/tree-sitter-c-sharp
 git clone https://github.com/tree-sitter/tree-sitter-haskell
+git clone https://github.com/tree-sitter-perl/tree-sitter-perl
+git clone https://github.com/tree-sitter/tree-sitter-ruby
 python3 build.py
